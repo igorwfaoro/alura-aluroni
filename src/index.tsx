970ec169f1d8027ff94c884a42123b8ffc404495
Igor Wilian Faoro <igor.faoro17@gmail.com>
2022-04-01 10:18:13 -0300

    feat: routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
-import Menu from './pages/Menu/Menu';
+import AppRouter from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Menu />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AppRouter></AppRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // https://github.com/alura-cursos/aluroni-introducao
+// https://github.com/alura-cursos/aluroni-router
